77f18b83e9fca5d235939d6c442940f4158c90b7
mark1foley <50077728+mark1foley@users.noreply.github.com>
2022-04-11 09:28:52 +1000

    Added update_frequency parameter Change sensor update logging level to "debug"

--- custom_components/gtfs_rt/sensor.py
+++ custom_components/gtfs_rt/sensor.py
@@ -25,6 +25,7 @@ CONF_STOP_ID = 'stopid'
 CONF_ROUTE = 'route'
 CONF_DEPARTURES = 'departures'
 CONF_TRIP_UPDATE_URL = 'trip_update_url'
+CONF_UPDATE_FREQUENCY = 'update_frequency'
 CONF_VEHICLE_POSITION_URL = 'vehicle_position_url'
 CONF_ROUTE_DELIMITER = 'route_delimiter'
 CONF_ICON = 'icon'
@@ -33,11 +34,12 @@ CONF_SERVICE_TYPE = 'service_type'
 DEFAULT_SERVICE = 'Service'
 DEFAULT_ICON = 'mdi:bus'
 
-MIN_TIME_BETWEEN_UPDATES = datetime.timedelta(seconds=60)
+MIN_TIME_BETWEEN_UPDATES = datetime.timedelta(seconds=CONF_UPDATE_FREQUENCY)
 TIME_STR_FORMAT = "%H:%M"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_TRIP_UPDATE_URL): cv.string,
+    vol.Optional(CONF_UPDATE_FREQUENCY, default=60): cv.positive_int,
     vol.Optional(CONF_API_KEY): cv.string,
     vol.Optional(CONF_VEHICLE_POSITION_URL): cv.string,
     vol.Optional(CONF_ROUTE_DELIMITER): cv.string,
@@ -60,7 +62,7 @@ def due_in_minutes(timestamp):
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Get the public transport sensor."""
     
-    data = PublicTransportData(config.get(CONF_TRIP_UPDATE_URL), config.get(CONF_VEHICLE_POSITION_URL), config.get(CONF_ROUTE_DELIMITER), config.get(CONF_API_KEY))
+    data = PublicTransportData(config.get(CONF_TRIP_UPDATE_URL), config.get(CONF_UPDATE_FREQUENCY), config.get(CONF_VEHICLE_POSITION_URL), config.get(CONF_ROUTE_DELIMITER), config.get(CONF_API_KEY))
     sensors = []
     for departure in config.get(CONF_DEPARTURES):
         sensors.append(PublicTransportSensor(
@@ -136,38 +138,39 @@ class PublicTransportSensor(Entity):
     def update(self):
         """Get the latest data from opendata.ch and update the states."""
         self.data.update()
-        _LOGGER.info("Sensor Update:")
-        _LOGGER.info("...Name: {0}".format(self._name))
-        _LOGGER.info("...{0}: {1}".format(ATTR_ROUTE,self._route))
-        _LOGGER.info("...{0}: {1}".format(ATTR_STOP_ID,self._stop))
-        _LOGGER.info("...{0}: {1}".format(ATTR_ICON,self._icon))
-        _LOGGER.info("...Service Type: {0}".format(self._service_type))
-        _LOGGER.info("...{0}: {1}".format("unit_of_measurement",self.unit_of_measurement))
-        _LOGGER.info("...{0}: {1}".format(ATTR_DUE_IN,self.state))
+        _LOGGER.debug("Sensor Update:")
+        _LOGGER.debug("...Name: {0}".format(self._name))
+        _LOGGER.debug("...{0}: {1}".format(ATTR_ROUTE,self._route))
+        _LOGGER.debug("...{0}: {1}".format(ATTR_STOP_ID,self._stop))
+        _LOGGER.debug("...{0}: {1}".format(ATTR_ICON,self._icon))
+        _LOGGER.debug("...Service Type: {0}".format(self._service_type))
+        _LOGGER.debug("...{0}: {1}".format("unit_of_measurement",self.unit_of_measurement))
+        _LOGGER.debug("...{0}: {1}".format(ATTR_DUE_IN,self.state))
         try:
-            _LOGGER.info("...{0}: {1}".format(ATTR_DUE_AT,self.extra_state_attributes[ATTR_DUE_AT]))
+            _LOGGER.debug("...{0}: {1}".format(ATTR_DUE_AT,self.extra_state_attributes[ATTR_DUE_AT]))
         except:
-            _LOGGER.info("...{0} not defined".format(ATTR_DUE_AT))
+            _LOGGER.debug("...{0} not defined".format(ATTR_DUE_AT))
         try:
-            _LOGGER.info("...{0}: {1}".format(ATTR_LATITUDE,self.extra_state_attributes[ATTR_LATITUDE]))
+            _LOGGER.debug("...{0}: {1}".format(ATTR_LATITUDE,self.extra_state_attributes[ATTR_LATITUDE]))
         except:
-            _LOGGER.info("...{0} not defined".format(ATTR_LATITUDE))
+            _LOGGER.debug("...{0} not defined".format(ATTR_LATITUDE))
         try:
-            _LOGGER.info("...{0}: {1}".format(ATTR_LONGITUDE,self.extra_state_attributes[ATTR_LONGITUDE]))
+            _LOGGER.debug("...{0}: {1}".format(ATTR_LONGITUDE,self.extra_state_attributes[ATTR_LONGITUDE]))
         except:
-            _LOGGER.info("...{0} not defined".format(ATTR_LONGITUDE))
+            _LOGGER.debug("...{0} not defined".format(ATTR_LONGITUDE))
         try:
-            _LOGGER.info("...Next {0}: {1}".format(self._service_type,self.extra_state_attributes["Next " + self._service_type]))
+            _LOGGER.debug("...Next {0}: {1}".format(self._service_type,self.extra_state_attributes["Next " + self._service_type]))
         except:
-            _LOGGER.info("...{0} not defined".format("Next " + self._service_type))
+            _LOGGER.debug("...{0} not defined".format("Next " + self._service_type))
 
 
 class PublicTransportData(object):
     """The Class for handling the data retrieval."""
 
-    def __init__(self, trip_update_url, vehicle_position_url=None, route_delimiter=None, api_key=None):
+    def __init__(self, trip_update_url, update_frequency, vehicle_position_url=None, route_delimiter=None, api_key=None):
         """Initialize the info object."""
         self._trip_update_url = trip_update_url
+        self._update_frequency = update_frequency
         self._vehicle_position_url = vehicle_position_url
         self._route_delimiter = route_delimiter
         if api_key is not None:
@@ -179,6 +182,7 @@ class PublicTransportData(object):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         _LOGGER.info("trip_update_url: {}".format(self._trip_update_url))
+        _LOGGER.info("update_frequency: {}".format(self._update_frequency))
         _LOGGER.info("vehicle_position_url: {}".format(self._vehicle_position_url))
         _LOGGER.info("route_delimiter: {0}".format(self._route_delimiter))
         _LOGGER.info("header: {0}".format(self._headers))
